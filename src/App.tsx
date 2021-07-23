415ffef53bbc2359a54366d871aae9ed6d9b2468
Hector Castro <hectcastro@gmail.com>
2021-07-23 07:52:28 -0400

    feat: add ability to type, reset, and delete

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,36 @@ import { Container, Heading, Hero } from "react-bulma-components";
 import { useHotkeys } from "react-hotkeys-hook";
 import "./App.css";
 
+const resetKeys = ["Enter", "Escape"];
+
+function isAlphaNumeric(key: string) {
+  return key.match(/^[a-z0-9]$/i);
+}
+
 function App() {
-  const [state, setState] = useState("?");
+  const [state, setState] = useState("");
   useHotkeys("*", (event: KeyboardEvent) =>
     setState((prevState) => {
       const newState = event.key;
-      return newState.match(/^[a-z0-9]$/i) ? newState : prevState;
+
+      if (resetKeys.includes(newState)) {
+        return "";
+      }
+
+      if (newState === "Backspace") {
+        return prevState.slice(0, -1);
+      }
+
+      if (isAlphaNumeric(newState)) {
+        const totalState = prevState + newState;
+        if (totalState.length <= 10) {
+          return prevState + newState;
+        }
+
+        return totalState.substr(1);
+      }
+
+      return prevState;
     })
   );
 
